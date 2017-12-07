1ca426baba9342c88696b8f5c56aa1c651f744d1
Thomas Pauli <t.pauli@nexbyte.com>
2017-12-07 09:52:43 +0100

    Fix hackmd docker and do some changes in the config file

---
+++ docker-entrypoint.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+if [ "$HMD_DB_URL" = "" ]; then
+  HMD_DB_URL="mysql://root:nexbyte123@db:3306/hackmd"
+fi
+
+DB_SOCKET=$(echo ${HMD_DB_URL} | sed -e 's/.*:\/\//\/\//' -e 's/.*\/\/[^@]*@//' -e 's/\/.*$//')
+
+echo $HMD_DB_URL
+
+echo $DB_SOCKET
+
+if [ "$DB_SOCKET" != "" ]; then
+    dockerize -wait tcp://${DB_SOCKET} -timeout 30s
+fi
+
+node_modules/.bin/sequelize db:migrate
+
+# Print warning if local data storage is used but no volume is mounted
+[ "$HMD_IMAGE_UPLOAD_TYPE" = "filesystem" ] && { mountpoint -q ./public/uploads || {
+    echo "
+        #################################################################
+        ###                                                           ###
+        ###                        !!!WARNING!!!                      ###
+        ###                                                           ###
+        ###        Using local uploads without persistence is         ###
+        ###            dangerous. You'll loose your data on           ###
+        ###              container removal. Check out:                ###
+        ###  https://docs.docker.com/engine/tutorials/dockervolumes/  ###
+        ###                                                           ###
+        ###                       !!!WARNING!!!                       ###
+        ###                                                           ###
+        #################################################################
+    ";
+} ; }
+
+# Sleep to make sure everything is fine...
+sleep 3
+
+# run
+node app.js
