3849304e1af27e7b33c129d70e6989e42841fd3c
Thomas Pauli <t.pauli@nexbyte.com>
2017-12-12 09:36:35 +0100

    Temporary remove the db check function from the entrypoint shell script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,19 +1,5 @@
 #!/bin/sh
 
-if [ ! -z "$HMD_DB_URL" ]; then
-  DB_SOCKET=$(echo ${HMD_DB_URL} | sed -e 's/.*:\/\//\/\//' -e 's/.*\/\/[^@]*@//' -e 's/\/.*$//')
-
-  echo $HMD_DB_URL
-  echo $DB_SOCKET
-
-  if [ "$DB_SOCKET" != "" ]; then
-      dockerize -wait tcp://${DB_SOCKET} -timeout 30s
-  fi
-
-  # Run the migration scripts!
-  node_modules/.bin/sequelize db:migrate
-fi
-
 # Print warning if local data storage is used but no volume is mounted
 [ "$HMD_IMAGE_UPLOAD_TYPE" = "filesystem" ] && { mountpoint -q ./public/uploads || {
     echo "
